980f2eb56bf9e05e6773aa51f88038cbbe7f5db4
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-11-09 14:27:51 +0100

    :sparkles: Legger til test schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,20 @@
-import {defineConfig} from 'sanity'
+import {AuthConfig, defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+
+const auth: AuthConfig = {
+  redirectOnSingle: true,
+  providers: () => [
+    {
+      name: 'saml',
+      title: 'NAV SSO',
+      url: 'https://api.sanity.io/v2021-10-01/auth/saml/login/f3270b37',
+      logo: '/static/navlogo.svg',
+    },
+  ],
+  loginMethod: 'dual',
+};
 
 export default defineConfig({
   name: 'default',
@@ -12,6 +25,7 @@ export default defineConfig({
 
   plugins: [deskTool(), visionTool()],
 
+  auth: auth,
   schema: {
     types: schemaTypes,
   },
---
+++ schemas/brev.ts
@@ -0,0 +1,18 @@
+
+export default {
+  name: 'brev',
+  type: 'document',
+  title: 'Brevmal',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name'
+    },
+    {
+      name: 'content',
+      type: 'string',
+      title: 'Content'
+    }
+  ]
+}
