90f89cfe0ac2a56ee935481d74af0fb46064962c
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-11-14 12:43:09 +0100

    Legger til block som content i brev

--- schemas/brev.ts
+++ schemas/brev.ts
@@ -1,18 +1,23 @@
+import {defineType} from '@sanity/types'
+import {defineField} from 'sanity'
 
-export default {
+const brevmal = defineType({
   name: 'brev',
   type: 'document',
   title: 'Brevmal',
   fields: [
-    {
+    defineField({
       name: 'name',
       type: 'string',
-      title: 'Name'
-    },
-    {
+      title: 'Name',
+    }),
+    defineField({
+      title: 'Content',
       name: 'content',
-      type: 'string',
-      title: 'Content'
-    }
-  ]
-}
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+  ],
+})
+
+export default brevmal
