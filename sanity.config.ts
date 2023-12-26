b9436c3b1aa0ce4456f05dbf577efc4ba492e2e4
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2023-12-26 13:13:44 +0100

    :sparkles: Legger til støtte for presentation og locate slik at redaktører visuelt kan se i hvilke brevmaler en standardtekst blir brukt

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,12 @@
 import { AuthConfig, defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
+import { presentationTool } from 'sanity/presentation';
+
 import { schemaTypes } from './schemas';
+import { locate } from './locate';
+
+const SANITY_PREVIEW_URL = 'http://localhost:3000';
 
 const auth: AuthConfig = {
   redirectOnSingle: true,
@@ -23,7 +28,14 @@ export default defineConfig({
   projectId: '948n95rd',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool(),
+    visionTool(),
+    presentationTool({
+      previewUrl: SANITY_PREVIEW_URL,
+      locate: locate,
+    }),
+  ],
 
   auth: auth,
   schema: {
