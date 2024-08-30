71f88ab35a9f6036314d249200ac6be5a8173453
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2024-08-30 14:02:18 +0200

    :tada: Next init slik at vi kan embedde Sanity i nextjs app og deploye til NAIS

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,2 @@
+self.__RSC_SERVER_MANIFEST =
+  '{\n  "node": {},\n  "edge": {},\n  "encryptionKey": "BtjdbNbo0ly4cwJDy0jRmk3fvUeGJfd3vv5/k3MEnOM="\n}';
---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  trailingSlash: true,
+  reactStrictMode: true,
+  output: 'standalone',
+  assetPrefix: process.env.ASSET_PREFIX ?? undefined,
+
+  experimental: {
+    instrumentationHook: true,
+    optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons'],
+  },
+
+  images: {
+    formats: ['image/webp'],
+  },
+};
+
+module.exports = nextConfig;
