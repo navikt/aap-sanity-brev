61c6c14456609c029948a59a52154a1de7ed53cb
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2024-08-30 17:15:42 +0200

    Legger på styling fra designsystem og lister ut mer innhold fra brevmalene

--- components/brevbygger/Brevbygger.tsx
+++ components/brevbygger/Brevbygger.tsx
@@ -1,11 +1,26 @@
 'use client';
 
+import { Heading } from '@navikt/ds-react';
 import { Brevmal } from 'lib/services/sanity/model/brevmal/brevmalQuery';
 
 export const Brevbygger = ({ brevmal }: { brevmal: Brevmal }) => {
   return (
     <div>
-      <h1>{brevmal.brevtittel}</h1>
+      <Heading level="1" size="xlarge">
+        {brevmal.brevtittel}
+      </Heading>
+      {brevmal.innhold.map((innhold) => {
+        if (innhold._type === 'standardtekst') {
+          return (
+            <div key={innhold._id}>
+              <Heading level="2" size="large">
+                {innhold.overskrift}
+              </Heading>
+            </div>
+          );
+        }
+        return null;
+      })}
     </div>
   );
 };
