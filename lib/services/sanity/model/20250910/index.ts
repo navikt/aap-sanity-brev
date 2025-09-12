74bcca082b8605ce4acc018d191338e4d96fa843
Matias Vinjevoll <matias.vinjevoll@nav.no>
2025-09-12 12:39:43 +0200

    Legg til schemaTypes

---
+++ lib/services/sanity/model/20250910/index.ts
@@ -0,0 +1,11 @@
+import {fritekst} from './fritekst'
+import {tekst} from './tekst'
+import {valg} from './valg'
+import {delmal} from './delmal'
+import {mal} from './mal'
+import {faktagrunnlag} from './faktagrunnlag'
+import {periodetekst} from './periodetekst'
+import {valgGruppe} from './valgGruppe'
+import {gruppertTekstRef} from './gruppertTekstRef'
+
+export const schemaTypes = [tekst, fritekst, valg, mal, delmal, faktagrunnlag, periodetekst, valgGruppe, gruppertTekstRef]
