02dc8113982fa1565645529cebbd92e4c1458ddf
Matias Vinjevoll <matias.vinjevoll@nav.no>
2025-09-17 14:12:31 +0200

    Fjern periodetekst fra tekst

--- lib/services/sanity/model/20250910/tekst.ts
+++ lib/services/sanity/model/20250910/tekst.ts
@@ -1,5 +1,4 @@
 import {defineField, defineType} from 'sanity'
-import {periodetekstRef} from './periodetekstRef'
 
 export const tekst = defineType({
   title: 'Tekst',
@@ -16,7 +15,6 @@ export const tekst = defineType({
       name: 'teksteditor',
       type: 'array',
       of: [
-        periodetekstRef,
         {
           type: 'block',
           of: [
