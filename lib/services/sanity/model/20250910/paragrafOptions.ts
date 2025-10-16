25afabe488ddc564d89045bf42f7d17231bdd421
Øivind Stensrud <oivind.stensrud@nav.no>
2025-10-16 16:32:40 +0200

    Kategori for kapittel 22

--- lib/services/sanity/model/20250910/paragrafOptions.ts
+++ lib/services/sanity/model/20250910/paragrafOptions.ts
@@ -29,41 +29,43 @@ export type Paragraf =
   | '11-29'
   | '11-30'
   | '11-31'
+  | '22';
 
 export type ParagrafOption = {
-  title: string
-  value: Paragraf
-}
+  title: string;
+  value: Paragraf;
+};
 
 export const paragrafOptions: ParagrafOption[] = [
-  {title: '11-2', value: '11-2'},
-  {title: '11-3', value: '11-3'},
-  {title: '11-4', value: '11-4'},
-  {title: '11-5', value: '11-5'},
-  {title: '11-6', value: '11-6'},
-  {title: '11-7', value: '11-7'},
-  {title: '11-8', value: '11-8'},
-  {title: '11-9', value: '11-9'},
-  {title: '11-10', value: '11-10'},
-  {title: '11-11', value: '11-11'},
-  {title: '11-12', value: '11-12'},
-  {title: '11-13', value: '11-13'},
-  {title: '11-14', value: '11-14'},
-  {title: '11-15', value: '11-15'},
-  {title: '11-16', value: '11-16'},
-  {title: '11-17', value: '11-17'},
-  {title: '11-18', value: '11-18'},
-  {title: '11-19', value: '11-19'},
-  {title: '11-20', value: '11-20'},
-  {title: '11-21', value: '11-21'},
-  {title: '11-22', value: '11-22'},
-  {title: '11-23', value: '11-23'},
-  {title: '11-24', value: '11-24'},
-  {title: '11-25', value: '11-25'},
-  {title: '11-26', value: '11-26'},
-  {title: '11-27', value: '11-27'},
-  {title: '11-28', value: '11-28'},
-  {title: '11-29', value: '11-29'},
-  {title: '11-30', value: '11-30'},
-  {title: '11-31', value: '11-31'},
-]
+  { title: '11-2', value: '11-2' },
+  { title: '11-3', value: '11-3' },
+  { title: '11-4', value: '11-4' },
+  { title: '11-5', value: '11-5' },
+  { title: '11-6', value: '11-6' },
+  { title: '11-7', value: '11-7' },
+  { title: '11-8', value: '11-8' },
+  { title: '11-9', value: '11-9' },
+  { title: '11-10', value: '11-10' },
+  { title: '11-11', value: '11-11' },
+  { title: '11-12', value: '11-12' },
+  { title: '11-13', value: '11-13' },
+  { title: '11-14', value: '11-14' },
+  { title: '11-15', value: '11-15' },
+  { title: '11-16', value: '11-16' },
+  { title: '11-17', value: '11-17' },
+  { title: '11-18', value: '11-18' },
+  { title: '11-19', value: '11-19' },
+  { title: '11-20', value: '11-20' },
+  { title: '11-21', value: '11-21' },
+  { title: '11-22', value: '11-22' },
+  { title: '11-23', value: '11-23' },
+  { title: '11-24', value: '11-24' },
+  { title: '11-25', value: '11-25' },
+  { title: '11-26', value: '11-26' },
+  { title: '11-27', value: '11-27' },
+  { title: '11-28', value: '11-28' },
+  { title: '11-29', value: '11-29' },
+  { title: '11-30', value: '11-30' },
+  { title: '11-31', value: '11-31' },
+  { title: '22', value: '22' },
+];
