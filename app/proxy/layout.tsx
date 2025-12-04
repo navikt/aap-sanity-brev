8e7c086e49bc5befba6cc39582861b53c2443626
Tor Idland <90849097+tidnav@users.noreply.github.com>
2025-12-04 07:15:25 +0100

    react 19.2.1 og fjern aap-felles-react (#194)

--- app/proxy/layout.tsx
+++ app/proxy/layout.tsx
@@ -1,5 +1,4 @@
 import '@navikt/ds-css';
-import '@navikt/aap-felles-css';
 import 'packages/aap-breveditor-css/src/breveditor.css';
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
